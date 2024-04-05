0407ed44b1cd58b62375aaa7bc5dfabdadcc34f0
Suptika2 <136931230+suman-3@users.noreply.github.com>
2024-04-05 08:56:10 +0000

    image done

--- client/src/components/Doctor.js
+++ client/src/components/Doctor.js
@@ -5,40 +5,36 @@ function Doctor({ doctor }) {
   const navigate = useNavigate();
   return (
     <div
-      className="card p-2 my-2 cursor-pointer"
+      className="card p-2 cursor-pointer"
       onClick={() => navigate(`/book-appointment/${doctor._id}`)}
-      style={{ position: 'relative' }}
     >
-      <div className="imageBox" style={{ position: 'absolute', left: 10, top: 10 }}>
-        {doctor.profileimg && <img src={doctor.profileimg} alt="doctor image" width="100%"
-          height="400" />}
+      <div className="profileImg">
+        <img src={doctor.profileimg} alt="Doctor Profile" width="100%" height="240px" />
       </div>
-      <div style={{ marginLeft: 120 }}>
-        <h1 className="card-title">
-          {doctor.firstName} {doctor.lastName}
-        </h1>
-        <p>
-          <b>Specialization : </b>
-          {doctor.specialization}
-        </p>
-        <hr />
-        <p>
-          <b>Phone Number : </b>
-          {doctor.phoneNumber}
-        </p>
-        <p>
-          <b>Address : </b>
-          {doctor.address}
-        </p>
-        <p>
-          <b>Fee per Visit : </b>
-          {doctor.feePerCunsultation}
-        </p>
-        <p>
-          <b>Timings : </b>
-          {doctor.timings[0]} - {doctor.timings[1]}
-        </p>
-      </div>
+      <h1 className="card-title">
+        {doctor.firstName} {doctor.lastName}
+      </h1>
+      <p>
+        <b>Specialization : </b>
+        {doctor.specialization}
+      </p>
+      <hr />
+      <p>
+        <b>Phone Number : </b>
+        {doctor.phoneNumber}
+      </p>
+      <p>
+        <b>Address : </b>
+        {doctor.address}
+      </p>
+      <p>
+        <b>Fee per Visit : </b>
+        {doctor.feePerCunsultation}
+      </p>
+      <p>
+        <b>Timings : </b>
+        {doctor.timings[0]} - {doctor.timings[1]}
+      </p>
     </div>
   );
 }
